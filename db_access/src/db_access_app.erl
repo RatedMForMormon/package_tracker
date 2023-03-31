cf279e3df3fc18937dc4d077f3970dca245556b2
Christian Viazzo <via17001@byui.edu>
2023-03-31 05:55:09 -0600

    added pathing to endpoints

--- db_access/src/db_access_app.erl
+++ db_access/src/db_access_app.erl
@@ -14,7 +14,9 @@ start(_Type, _Args) ->
 		{'_', [
 		    {"/", toppage_h, []},
 		    {"/store_package_info", store_package_info, []},
-		    {"/query_package_history", query_package_history, []}
+		    {"/query_package_history", query_package_history, []},
+			{"/store_vehicle_info", store_vehicle_info, []},
+			{"/query_vehicle_history", query_vehicle_history, []}
 		]}
 	]),
 	PrivDir = code:priv_dir(db_access),
