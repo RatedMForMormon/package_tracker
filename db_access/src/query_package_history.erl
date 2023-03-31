0d17e8bc9dbe70738d9464c3b01128ee109c3d10
chvia223 <via17001@byui.edu>
2023-03-31 11:41:11 +0000

    reformatted jsx

--- db_access/src/query_package_history.erl
+++ db_access/src/query_package_history.erl
@@ -6,7 +6,9 @@ init(Req0, Opts) ->
 	{ok,Data,_} = cowboy_req:read_body(Req0),
 	#{<<"package_id">> := Package_id} = jsx:decode(Data),
 	{ok, Response} = package_tracker:query_package_history(package_tracker, Package_id),
-	Decoded = jsx:encode(binary_to_term(riakc_obj:get_value(Response))),
+	io:format("Riak Content: ~p~n", [binary_to_term(riakc_obj:get_value(Response))]),
+	Decoded = jsx:encode(#{history => binary_to_term(riakc_obj:get_value(Response))}),
+		    %binary_to_term(riakc_obj:get_value(Response))),
         Req = cowboy_req:reply(200, #{
                 <<"content-type">> => <<"text/json">>
         }, Decoded, Req0),
